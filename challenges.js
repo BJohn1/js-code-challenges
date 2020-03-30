947b25e7ad135a3c931eb0d91de19affe6ddaa21
Blake <b.adeyemijohn@gmail.com>
2020-03-29 18:06:25 -0700

    solve problem 29

--- challenges.js
+++ challenges.js
@@ -1084,9 +1084,22 @@ addChecker( [10, 15, 16, 22], 19 ) // => false
 -----------------------------------------------------------------*/
 // Your solution for 29-addChecker here:
 
-
-
-
+function addChecker(arr,int){
+  let count=0
+  for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+      if((arr[i]+arr[j])===int){
+        count+=1
+      }
+    }
+  }
+  if(count>0){
+    return true
+  }
+  else{
+    return false
+  }
+}
 
 /*-----------------------------------------------------------------
 Challenge: 30-totalTaskTime
